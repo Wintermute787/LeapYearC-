6d5628aef2b6a25e1f4eb9d67846174343b66056
Joe Barnes and Michael Larragueta <michael@>
2019-04-22 15:31:00 -0700

    Add everything

---
+++ Calendar/Calendar/Models/LeapYear.cs
@@ -0,0 +1,14 @@
+﻿namespace Calendar
+{
+    public class LeapYear
+    {
+        public bool IsLeapYear(int year)
+        {
+            if (year % 100 == 0 && year % 400 != 0)
+            {
+                return false;
+            }
+            else return year % 4 == 0;
+        }
+    }
+}
